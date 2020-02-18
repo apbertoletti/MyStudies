83565af3011c4dfdfa21e143003893f77b487946
André P. Bertoletti <apbertoletti@gmail.com>
2020-02-18 08:08:32 -0300

    Adding new JQuery lesson - alter css

---
+++ JQuery/Lessons/02/myScritps.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#btnFrase1').click(function(){
+        $('#frase1').css("color", "blue");
+    });
+    $('#btnFrase2').click(function(){
+        $('#frase2').css("color", "red");
+    })
+});
