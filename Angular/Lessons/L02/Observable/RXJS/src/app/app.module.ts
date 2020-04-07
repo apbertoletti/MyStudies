acad035602fed00e3b526453ac8d2c89e208573c
André P. Bertoletti <apbertoletti@gmail.com>
2020-04-07 10:06:24 -0300

    Testando o uso de promisses

---
+++ Angular/Lessons/L02/Observable/RXJS/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, OnInit } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
