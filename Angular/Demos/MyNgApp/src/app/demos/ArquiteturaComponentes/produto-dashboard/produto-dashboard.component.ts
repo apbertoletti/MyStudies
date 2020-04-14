5b64af336ec04ca7b893a708b9ed03fc01eeda92
André P. Bertoletti <apbertoletti@gmail.com>
2020-04-14 01:04:38 -0300

    Decorators ViewChild e ViewChildren

--- Angular/Demos/MyNgApp/src/app/demos/ArquiteturaComponentes/produto-dashboard/produto-dashboard.component.ts
+++ Angular/Demos/MyNgApp/src/app/demos/ArquiteturaComponentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ViewChildren, Query, QueryList } from '@angular/core';
 import { Produto } from '../models/produto';
+import { Observable, fromEvent } from 'rxjs';
+import { ProdutoCountComponent } from '../componentes/produto-count.component';
+import { ProdutoDetalheComponent } from '../componentes/produto-card-detalhe.component';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html',
   styles: []
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
 
   produtos: Produto[]
 
+  @ViewChildren(ProdutoDetalheComponent)
+  produtoCards: QueryList<ProdutoDetalheComponent> 
+  
+  @ViewChild(ProdutoCountComponent, { static: false })
+  mensagemContador: ProdutoCountComponent
+
+  @ViewChild('cabecalhoProdutos', { static: false })
+  mensagemTela: ElementRef
+
   constructor() { }
+
+  ngAfterViewInit(): void {
+    //Podemos interagir com o estado do objeto, ou mesmo ele por completo.
+    console.log('Objeto do contador: ', this.mensagemContador.produtos);
+
+    let clickCabecalho: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click');
+    clickCabecalho.subscribe(() => {
+      alert('Clicou no texto!');
+      return;
+    })
+
+    console.log(this.produtoCards);
+    this.produtoCards.forEach(p => {
+      console.log(p.produto);
+    })
+  }
 
   ngOnInit() {
     this.produtos = [{
