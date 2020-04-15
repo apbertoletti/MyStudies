7c0d54fc3199ee55dd9b68542679c2ca6017d071
André P. Bertoletti <apbertoletti@gmail.com>
2020-04-15 00:43:19 -0300

    Tratamento do 404 (not found) das navegação entre as páginas

--- Angular/Demos/MyNgApp/src/app/app.routes.ts
+++ Angular/Demos/MyNgApp/src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
     { path: 'sobre', component: SobreComponent },
     { path: 'cadastro', component: CadastroComponent },
-    { path: 'produto', loadChildren: () => import('./demos/ArquiteturaComponentes/produto.module').then(m => m.ProdutoModule) }
+    { path: 'produto', loadChildren: () => import('./demos/ArquiteturaComponentes/produto.module').then(m => m.ProdutoModule) },
+
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
