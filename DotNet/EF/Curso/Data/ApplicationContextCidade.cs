4932195018f110f1932071b15687dae263dae8da
André P. Bertoletti <apbertoletti@gmail.com>
2020-12-03 23:23:31 -0300

    EnsureCreate and EnsureDelete

--- DotNet/EF/Curso/Data/ApplicationContextCidade.cs
+++ DotNet/EF/Curso/Data/ApplicationContextCidade.cs
@@ -5,10 +5,9 @@ using Microsoft.Extensions.Logging;
 
 namespace Curso.Data
 {
-    public class ApplicationContext : DbContext
+    public class ApplicationContextCidade : DbContext
     {
-        public DbSet<Funcionario> Funcionarios { get; set; }
-        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Cidade> Cidades { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
--- DotNet/EF/Curso/Program.cs
+++ DotNet/EF/Curso/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace DominadoEFCore
 {
@@ -6,7 +8,28 @@ namespace DominadoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //EnsureCreatingAndDeleting();
+            GapEnsureCreated();
+        }
+
+        static void EnsureCreatingAndDeleting()
+        {
+            using var db = new Curso.Data.ApplicationContext();
+            //db.Database.EnsureCreated();
+            //db.Database.EnsureDeleted();
+        }
+
+        static void GapEnsureCreated()
+        {
+            using var db1 = new Curso.Data.ApplicationContext();
+            using var db2 = new Curso.Data.ApplicationContextCidade();
+
+            db1.Database.EnsureCreated();
+            db2.Database.EnsureCreated();
+
+            //Abordagem para o EF conseguir criar os objetos do segundo contexto delimitado no BD
+            var databaseCreator = db2.GetService<IRelationalDatabaseCreator>();
+            databaseCreator.CreateTables();
         }
     }
 }
