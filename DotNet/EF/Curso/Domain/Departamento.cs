1f7e3aef4dd58612cc6a6dacb2f672e6190b7a60
André P. Bertoletti <apbertoletti@gmail.com>
2020-12-06 12:35:29 -0300

    Lazy Loading example 3 (Action approach)

--- DotNet/EF/Curso/Domain/Departamento.cs
+++ DotNet/EF/Curso/Domain/Departamento.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Curso.Domain
 {
@@ -9,20 +9,25 @@ namespace Curso.Domain
         {
         }
 
-        private ILazyLoader _lazyLoader {get;set;}
-        private Departamento(ILazyLoader lazyLoader)
+        private Action<object,string> _lazyLoader {get;set;}
+        private Departamento(Action<object,string> lazyLoader)
         {
             _lazyLoader = lazyLoader;
         }
 
         public int Id { get; set; }
         public string Descricao { get; set; }
-        public bool Ativo { get; set; }
+        public bool Ativo { get; set; }        
 
         private List<Funcionario> _funcionarios;
         public List<Funcionario> Funcionarios 
         {
-            get => _lazyLoader.Load(this, ref _funcionarios);
+            get
+            {
+                _lazyLoader?.Invoke(this, nameof(Funcionarios));
+
+                return _funcionarios;
+            } 
             set => _funcionarios = value;
         }
     }
