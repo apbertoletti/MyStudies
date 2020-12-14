8972bd023d876ef622b57b4ca8523d399a05c0f3
André P. Bertoletti <apbertoletti@gmail.com>
2020-12-14 18:08:23 -0300

    Instalando e usando o Express

--- Firebase/Demos/Todo/Api/functions/index.js
+++ Firebase/Demos/Todo/Api/functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ success: true })
+})
+
+app.get('/teste', (req, res) => {
+    res.json({ success: true, message: 'Deu bom!' })
+})
+
+exports.api = functions.https.onRequest(app);
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.json({ success: true });
-});
