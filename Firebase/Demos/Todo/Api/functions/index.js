29f70e0abf23ceb0106e8e19ccc5b52e96fe5f6b
André P. Bertoletti <apbertoletti@gmail.com>
2020-12-15 09:49:17 -0300

    Criando as tarefas via API

--- Firebase/Demos/Todo/Api/functions/index.js
+++ Firebase/Demos/Todo/Api/functions/index.js
@@ -1,5 +1,12 @@
 const functions = require('firebase-functions');
-const express = require('express')
+const express = require('express');
+const admin = require('firebase-admin');
+
+const serviceAccont = require('./todolist-73aea-firebase-adminsdk-zvk4b-a6d5530af1.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccont)
+})
 
 const app = express();
 
@@ -9,6 +16,14 @@ app.get('/', (req, res) => {
 
 app.get('/teste', (req, res) => {
     res.json({ success: true, message: 'Deu bom!' })
+})
+
+app.post('/createTask', async (req, res) => {
+    await admin.firestore().collection('Tasks').add({
+        text: req.body.text,
+        done: false
+    });
+    res.json({ success: true });
 })
 
 exports.api = functions.https.onRequest(app);
