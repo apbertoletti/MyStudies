c5d95374ab35a7e22cdf45a2d02874dfa1a4c2dd
André P. Bertoletti <apbertoletti@gmail.com>
2020-12-15 12:08:36 -0300

    Hosting e deploy da aplicação web

--- Firebase/Demos/Todo/Web/src/environments/environment.prod.ts
+++ Firebase/Demos/Todo/Web/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB0pVrn057AT8WWzD2-47yCz_SLCNhkS6c",
+    authDomain: "todolist-73aea.firebaseapp.com",
+    projectId: "todolist-73aea",
+    storageBucket: "todolist-73aea.appspot.com",
+    messagingSenderId: "302237195625",
+    appId: "1:302237195625:web:4885efb8fd2be2578e71f8",
+    measurementId: "G-4B58YZMY92"
+  }
 };
